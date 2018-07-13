a1cc1e5be5163530822bb61ddf114feff14aa921
tylerzhao <401469524@qq.com>
2018-07-13 16:02:43 +0800

    test

--- brand2id.py
+++ brand2id.py
@@ -24,4 +24,4 @@ for file in os.listdir(path):
         #     os.rename(os.path.join(path,file),os.path.join(path,newname))
         #     print (file,'ok')
 #        print file.split('.')[-1]
-# 123
+# 1234
